e0e78b14e9cd7f1dd74c41255365935468de3b96
rafaScalet <rafaelscalet2018@gmail.com>
2024-11-28 15:41:20 -0300

    add git and curl

--- installation.sh
+++ installation.sh
@@ -2,4 +2,4 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install nala docker.io docker-compose fish code neofetch tldr 
+sudo apt install nala docker.io docker-compose fish code neofetch tldr git curl flatpak
