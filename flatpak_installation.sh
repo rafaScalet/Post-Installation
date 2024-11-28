b5fa26f0a21409e24ab85269d14005fbc3dedc4d
rafaScalet <rafaelscalet2018@gmail.com>
2024-11-28 10:15:09 -0300

    flatpaks installation

---
+++ flatpak_installation.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+flatpak install flathub com.spotify.Client
+flatpak install flathub com.brave.Browser
+flatpak install flathub com.valvesoftware.Steam
+flatpak install flathub org.mozilla.Thunderbird
+flatpak install flathub org.heroicgameslauncher.hgl
+flatpak install flathub org.localsend.localsend_app
+
+flatpak install flathub com.usebruno.Bruno
+flatpak install flathub org.gaphor.Gaphor
+flatpak install flathub org.dbgate.DbGate
+
+# flatpak install flathub page.kramo.Cartridges
+
+# flatpak install flathub net.lutris.Lutris
+# flatpak install flathub com.usebottles.bottles
+# flatpak install flathub org.sugarlabs.Paint
